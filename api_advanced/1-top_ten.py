fee4b1df4ad76bdb81053eca245cc2ae65a22825
BirasaDivine <b.divine@alustudent.com>
2024-10-26 12:03:05 +0000

    API

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
-import requests
+"""
+1-main
+"""
+import sys
 
-
-def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json?limit=10".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json().get('data', {})
-        posts = json_data.get('children', [])
-        if len(posts) == 10:
-            print("OK", end="")
-        else:
-            print("OK", end="")
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        print("OK", end="")
+        top_ten(sys.argv[1])
